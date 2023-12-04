31082cc7cd8ea549c532de1abeae9d438f444d15
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-12-04 06:05:15 -0500

    day 2

--- day2.cpp
+++ day2.cpp
@@ -4,19 +4,16 @@
 #include <vector>
 #include <sstream>
 #include <regex>
+#include <map>
 
 using namespace std;
-int main()
-{
-    ifstream infile("input/debuginput.txt");
-    vector<string> inputList{};
-    string line;
-    while (getline(infile, line))
-    {
-        inputList.push_back(line);
-    }
-    infile.close();
 
+int partOne(vector<string> inputList)
+{
+    int output = 0;
+    map<string, int> maxValues = {{"blue", 14},
+                                  {"red", 12},
+                                  {"green", 13}};
     for (string line : inputList)
     {
         regex game("Game\\s(\\d+)");
@@ -33,21 +30,89 @@ int main()
         smatch color_match;
         stringstream colorStream(line);
         string draw;
-        vector<pair<int, vector<pair<int, string>>>> draws{};
-        int draw_number = 1;
+        map<string, int> maxColor = {};
         while (getline(colorStream, draw, ';'))
         {
-            vector<pair<int, string>> colors{};
             while (regex_search(draw, color_match, color))
             {
                 int amount = stoi(color_match[1]);
                 string color = color_match[2];
-                colors.push_back({amount, color});
+                auto result = maxColor.emplace(color, amount);
+                if (!result.second)
+                {
+                    int val = maxColor.at(color);
+                    if (val < amount)
+                    {
+                        maxColor.at(color) = amount;
+                    }
+                }
                 draw = color_match.suffix().str();
             }
-            draws.push_back({draw_number++, colors});
         }
+        if (maxValues.at("green") < maxColor.at("green"))
+        {
+            continue;
+        }
+        if (maxValues.at("blue") < maxColor.at("blue"))
+        {
+            continue;
+        }
+        if (maxValues.at("red") < maxColor.at("red"))
+        {
+            continue;
+        }
+        output += id;
+    }
+    return output;
+}
+
+int partTwo(vector<string> inputList)
+{
+    int output = 0;
+    for (string line : inputList)
+    {
+        line = line.erase(0, line.find(':') + 2);
+
+        regex color("(\\d+)\\s(blue|red|green)");
+        smatch color_match;
+        stringstream colorStream(line);
+        string draw;
+        map<string, int> maxColor = {};
+        while (getline(colorStream, draw, ';'))
+        {
+            while (regex_search(draw, color_match, color))
+            {
+                int amount = stoi(color_match[1]);
+                string color = color_match[2];
+                auto result = maxColor.emplace(color, amount);
+                if (!result.second)
+                {
+                    int val = maxColor.at(color);
+                    if (val < amount)
+                    {
+                        maxColor.at(color) = amount;
+                    }
+                }
+                draw = color_match.suffix().str();
+            }
+        }
+        output += maxColor.at("red") * maxColor.at("green") * maxColor.at("blue");
     }
 
+    return output;
+}
+int main()
+{
+    ifstream infile("input/inputday2.txt");
+    vector<string> inputList{};
+    string line;
+    while (getline(infile, line))
+    {
+        inputList.push_back(line);
+    }
+    infile.close();
+
+    cout << partOne(inputList) << endl;
+    cout << partTwo(inputList) << endl;
     return 0;
 }
