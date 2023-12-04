4298acccdcb50637171545d380325a0876e0467d
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-12-04 09:28:44 -0500

    github copilote fixed day three i still dont
    understand what i was doing wrong

--- day3.cpp
+++ day3.cpp
@@ -1,19 +1,93 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
-#include <sstream>
+#include <string>
+#include <fstream>
+bool isSymbol(char c)
+{
+    return c != '.' && !isdigit(c);
+}
 
-using namespace std;
+bool isPartNumber(int i, int start, int end, const std::vector<std::string> &schematic)
+{
+    int rows = schematic.size();
+    int cols = schematic[0].size();
+
+    for (int dx = -1; dx <= 1; ++dx)
+    {
+        for (int dy = -1; dy <= 1; ++dy)
+        {
+            for (int j = start; j < end; ++j)
+            {
+                int x = i + dx;
+                int y = j + dy;
+
+                if (x >= 0 && x < rows && y >= 0 && y < cols && isSymbol(schematic[x][y]))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+// ...
+
 int main()
 {
-    ifstream infile("input/inputday1.txt");
-    vector<string> inputList{};
-    string line;
-    while (getline(infile, line))
+    // ...
+
+    std::vector<std::string> schematic;
+    std::ifstream file("input/inputday3.txt");
+
+    if (file.is_open())
     {
-        inputList.push_back(line);
+        std::string line;
+        while (std::getline(file, line))
+        {
+            schematic.push_back(line);
+        }
+        file.close();
     }
-    infile.close();
+    else
+    {
+        std::cout << "Unable to open file";
+        return 1; // Exit with an error code
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < schematic.size(); ++i)
+    {
+        for (int j = 0; j < schematic[i].size();)
+        {
+            if (isdigit(schematic[i][j]))
+            {
+                int end = j;
+
+                while (end < schematic[i].size() && isdigit(schematic[i][end]))
+                {
+                    ++end;
+                }
+
+                int partNumber = std::stoi(schematic[i].substr(j, end - j));
+
+                if (isPartNumber(i, j, end, schematic))
+                {
+                    sum += partNumber;
+                }
+
+                j = end;
+            }
+            else
+            {
+                ++j;
+            }
+        }
+    }
+
+    std::cout << "Sum: " << sum << std::endl;
+
     return 0;
 }
